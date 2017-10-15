e15b9eec2edb031fea07cdbcb4d97b63f7170608
aalexeewna@yandex.ru <aalexeewna@yandex.ru>
2017-10-15 03:33:44 +0300

    Добавлены тесты, Добавлены функции в Auth

--- app/auth.py
+++ app/auth.py
@@ -1,9 +1,83 @@
-"""
-Auth class
-"""
+import hashlib, sys
 
 
 class Auth(object):
+    """
+    Auth class
+    """
+
+    def add_user(self, login, password):
+        try:
+            file = open('app/users.txt', 'r')
+
+            if_exist = False
+
+            for line in file:
+                user = line.split('/')
+                if login == user[0]:
+                    if_exist = True
+                    break
+
+            if if_exist:
+                file.close()
+                return 1
+            else:
+                file.close()
+                file = open('app/users.txt', 'a')
+                new_str = '{}/{}/'.format(login, self.get_md5(password))
+                file.write(new_str + '\n')
+                file.close()
+                return 0
+        except IOError as e:
+            print("Invalid file.", e)
+            sys.exit()
+
+    def del_user(self, login, password):
+        try:
+            file = open('app/users.txt', 'r')
+
+            if_exist = False
+            l = 0
+
+            for line in file:
+                user = line.split('/')
+                if login == user[0]:
+                    if_exist = True
+                    break
+
+            if if_exist:
+                file.close()
+                return 0
+            else:
+                file.close()
+                return 1
+        except IOError as e:
+            print("Invalid file.", e)
+            sys.exit()
 
     def check_user(self, login, password):
-        return 1
+        try:
+            file = open('app/users.txt', 'r')
+
+            for line in file:
+                user = line.split('/')
+
+                if login == user[0]:
+                    if self.get_md5(password) == user[1]:
+                        file.close()
+                        return 1
+                    else:
+                        print(self.get_md5(password), user[1])
+                        file.close()
+                        return 3
+                else:
+                    file.close()
+                    return 2
+        except IOError:
+            print("Invalid file.")
+            sys.exit()
+
+    def get_md5(self, password):
+        m = hashlib.md5()
+        m.update(password.encode('utf-8'))
+        return m.hexdigest()
--- test/test_lab.py
+++ test/test_lab.py
@@ -11,12 +11,28 @@ class TddPythonLab(unittest.TestCase):
         self.assertEqual(1, result)
 
     def test_auth_error_user_not_found(self):
-        result = self.auth.check_user(login='user', password='password')
+        result = self.auth.check_user(login='user_ghost', password='password')
         self.assertEqual(2, result)
 
     def test_auth_error_password_error(self):
-        result = self.auth.check_user(login='user', password='password')
+        result = self.auth.check_user(login='user', password='password_error')
         self.assertEqual(3, result)
+
+    def test_create_user_success(self):
+        result = self.auth.add_user(login='user2', password='password2')
+        self.assertEqual(0, result)
+
+    def test_create_user_failed(self):
+        result = self.auth.add_user(login='user_new', password='password_new')
+        self.assertEqual(1, result)
+
+    def test_delete_user_success(self):
+        result = self.auth.del_user(login='user2', password='password2')
+        self.assertEqual(0, result)
+
+    def test_delete_user_failed(self):
+        result = self.auth.del_user(login='user_new', password='password_failed')
+        self.assertEqual(1, result)
 
 
 if __name__ == '__main__':
